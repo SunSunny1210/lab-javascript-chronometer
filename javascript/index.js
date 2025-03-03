74cbd94b0579d8cacaeafbcf77a77afe4015dbcf
Sun Sunny <sol16821@gmail.com>
2025-03-03 12:19:31 +0100

    Fixed right button change and erased repeated variables.

--- javascript/index.js
+++ javascript/index.js
@@ -42,10 +42,13 @@ function setStopBtn() {
   // ... your code goes here
   if (btnLeftElement.classList.contains('stop')) {
     chronometer.stop();
-    const element = document.querySelector('#btnLeft');
-    element.classList.remove('stop');
-    element.classList.add('start')
-    element.innerHTML = 'START'
+    btnLeftElement.classList.remove('stop');
+    btnLeftElement.classList.add('start');
+    btnLeftElement.innerHTML = 'START';
+
+    btnRightElement.classList.remove('reset');
+    btnRightElement.classList.add('split');
+    btnRightElement.innerHTML = 'SPLIT';
   }
 }
 
@@ -53,10 +56,8 @@ function setSplitBtn() {
   // ... your code goes here
   if (btnRightElement.classList.contains('split')) {
     chronometer.split();
-    const element = document.querySelector('#btnRight');
-    element.classList.remove('split');
-    element.classList.add('reset');
-    element.innerHTML = 'RESET';
+    
+    
   }
 }
 
@@ -64,25 +65,23 @@ function setStartBtn() {
   // ... your code goes here
   if (btnLeftElement.classList.contains('start')) {
     chronometer.start(() => {
-      const minUni = document.querySelector('#minUni');
-      const minDec = document.querySelector('#minDec');
-      const secUni = document.querySelector('#secUni');
-      const secDec = document.querySelector('#secDec');
-
       const computedMinutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
       const computedSeconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
  
-      minDec.innerHTML = computedMinutes[0];
-      minUni.innerHTML = computedMinutes[1];
+      minDecElement.innerHTML = computedMinutes[0];
+      minUniElement.innerHTML = computedMinutes[1];
 
-      secDec.innerHTML = computedSeconds[0];
-      secUni.innerHTML = computedSeconds[1];
+      secDecElement.innerHTML = computedSeconds[0];
+      secUniElement.innerHTML = computedSeconds[1];
     });
 
-    const element = document.querySelector('#btnLeft');
-    element.classList.remove('start');
-    element.classList.add('stop')
-    element.innerHTML = 'STOP'
+    btnLeftElement.classList.remove('start');
+    btnLeftElement.classList.add('stop');
+    btnLeftElement.innerHTML = 'STOP';
+
+    btnRightElement.classList.remove('split');
+    btnRightElement.classList.add('reset');
+    btnRightElement.innerHTML = 'RESET';
   }
 }
 
@@ -90,10 +89,8 @@ function setResetBtn() {
   // ... your code goes here
   if (btnRightElement.classList.contains('reset')) {
     chronometer.reset();
-    const element = document.querySelector('#btnRight');
-    element.classList.remove('reset');
-    element.classList.add('split')
-    element.innerHTML = 'SPLIT'
+    [minDecElement, minUniElement, secDecElement, secUniElement, milDecElement, milUniElement].forEach((el) => el.innerHTML = '0')
+    
   }
 }
 
